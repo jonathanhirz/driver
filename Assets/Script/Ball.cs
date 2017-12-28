e0deddec4b90cc492e6ce5a80587b565b2e0178c
Jonathan Hirz <jonathanhirz@gmail.com>
2017-12-27 21:26:13 -0800

    new sounds

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	[SerializeField]
+    private AudioClip ballHit;
+
+    void OnCollisionEnter(Collision coll) {
+        if(coll.relativeVelocity.magnitude >= 5) {
+            AudioSource.PlayClipAtPoint(ballHit, transform.position);
+        }
+    }
+}
---
+++ Assets/Script/Cube.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+	[SerializeField]
+    private AudioClip blockHit;
+
+    void OnCollisionEnter(Collision coll) {
+        if(coll.relativeVelocity.magnitude >= 5) {
+            AudioSource.PlayClipAtPoint(blockHit, transform.position);
+        }
+    }
+}
